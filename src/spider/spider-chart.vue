9b44551da5bfdcf2c7a2e34bd5255e7bd6f2605d
Ju <tjdus0735@naver.com>
2023-11-22 16:29:45 +0900

    changes

--- src/spider/spider-chart.vue
+++ src/spider/spider-chart.vue
@@ -1,350 +1,243 @@
 <template>
-    <div>
-		<v-row style="height:100vh;">
-			<div class="checkpoints-container">
-				<div v-for="part in perspectives" style="padding:16px">
-					<div>{{ part.partName }}</div>
-					<div v-for="partDetail in part.details">
-						<h2>{{ partDetail.criteria }}</h2>
-						<div v-for="(level, levelIndex) in partDetail.levels" :key="levelIndex">
-							<h3 class="clearfix" style="margin:15px 0px 2px 0px;">
-								<div style="float: left;">Level {{ levelIndex + 1}}</div>
-								<div v-if="level.Objective">&nbsp;({{ level.Objective }})</div>
-							</h3>
-							<label v-for="(checkpoint, checkpointIndex) in level.checkpoints" :key="checkpointIndex">
-							<div style="display:flex;">
-								<input type="checkbox" v-model="checkpoint.checked" @change="updateLevelCompletion(partDetail, level)">
-								<div style="margin:4px 0px 0px 4px;">{{ checkpoint.text }}</div>
-							</div>
-							</label>
-						</div>
-					</div>
-				</div>
-		
-				<div style="padding:0px 16px 10px 16px;">
-					<h2 style="margin-bottom:4px;">결과</h2>
-					<div v-if="isAllLevelsCompleted">모든 레벨이 완료되었습니다.</div>
-					<div v-else>일부 레벨이 완료되지 않았습니다.</div>
-				</div>
-			</div>
-			<v-card style="height:100%; flex:1;">
-				<User style="padding:20px 0px 20px 0px;"/>
-				<v-divider/>
-				<div v-for="part in perspectives"
-					class="clearfix"
-					style="margin:5px;"
-				>
-					<div style="width:16px; height:16px; float: left; border-radius: 5px; margin-right:5px;"
-						:style="{ backgroundColor: part.partColor }"
-					></div>
-					<div style="font-weight: 500;">{{ part.partName }}</div>
-				</div>
-				<svg :width="chartWidth" :height="chartHeight">
-					<g :transform="`translate(${chartCenterX}, ${chartCenterY})`">
-						<g v-for="(detail, index) in getAllDetail(perspectives)" :key="detail.detail.criteria">
-							<line
-								:x1="0"
-								:y1="0"
-								:x2="getCoordinate(chartRadius, index, getDataLength(perspectives))[0]"
-								:y2="getCoordinate(chartRadius, index, getDataLength(perspectives))[1]"
-								:stroke="detail.color"
-							/>
-							<text
-								:x="getCoordinate(chartRadius + labelOffset, index, getDataLength(perspectives))[0]"
-								:y="getCoordinate(chartRadius + labelOffset, index, getDataLength(perspectives))[1]"
-								dominant-baseline="middle"
-								text-anchor="middle"
-							>
-								{{ detail.detail.criteria }}
-							</text>
-						</g>
-						<g>
-							<polygon
-								:points="getPolygonPoints(getDataArray(perspectives))"
-								fill="rgba(75, 192, 192, 0.2)"
-								stroke="rgba(75, 192, 200, 1)"
-							/>
-							<circle
-								v-for="(value, index) in getDataArray(perspectives)"
-								:key="`data-${index}`"
-								:cx="getCoordinate(chartRadius * (value / maxDataValue), index, getDataLength(perspectives))[0]"
-								:cy="getCoordinate(chartRadius * (value / maxDataValue), index, getDataLength(perspectives))[1]"
-								:r="pointRadius"
-								fill="rgba(75, 192, 192, 1)"
-							/>
-						</g>
-					</g>
-				</svg>
-			</v-card>
-		</v-row>
-    </div>
-</template>
+	<div class="container">
+	  <div class="checkpoints-container" style="height:100vh; overflow:auto;">
+		<div v-for="(perspective, index) in perspectives" :key="index" style="padding:16px">
+		  <h2>{{ perspective.name }}</h2>
+		  <div v-for="(level, levelIndex) in perspective.levels" :key="levelIndex">
+			<h3 style="margin-bottom:4px;">Level {{ levelIndex + 1 }} :{{ level.name }}</h3>
+			  <label style="background-color:yellow;" v-for="(checkpoint, checkpointIndex) in level.checkpoints" :key="checkpointIndex">
+			  <div style="display:flex;">
+				<input type="checkbox" v-model="checkpoint.checked" @change="updateLevelCompletion(perspective, level)">
+				<div>{{ checkpoint.text }}</div>
+			  </div>
+			</label>
+		  </div>
+		</div>
   
-<script>
-import User from './user.vue'
-const _ = require('lodash');
-
-const LOCAL_STORAGE_KEY = 'checkpoints_state';
-
-export default {
-	name: 'spiderChart',
-	components: {
-		User
-	},
+		<div style="padding:0px 16px 10px 16px;">
+		  <h2 style="margin-bottom:4px;">Result</h2>
+		  <div v-if="isAllLevelsCompleted">All levels are completed!</div>
+		  <div v-else>Some levels are not completed.</div>
+		</div>
+	  </div>
+  
+	  <div class="chart-container" style="background-color:red">
+		<svg :width="chartWidth" :height="chartHeight" style="background-color:yellow; margin-top:20px;">
+		  <g :transform="`translate(${chartCenterX}, ${chartCenterY})`">
+			<!-- Draw axes -->
+			<g v-for="(axis, index) in chartData.labels" :key="index">
+			  <line
+				:x1="0"
+				:y1="0"
+				:x2="getCoordinate(chartRadius, index, chartData.labels.length)[0]"
+				:y2="getCoordinate(chartRadius, index, chartData.labels.length)[1]"
+				stroke="lightgray"
+			  />
+			  <text
+				:x="getCoordinate(chartRadius + labelOffset, index, chartData.labels.length)[0]"
+				:y="getCoordinate(chartRadius + labelOffset, index, chartData.labels.length)[1]"
+				dominant-baseline="middle"
+				text-anchor="middle"
+			  >
+				{{ axis }}
+			  </text>
+			</g>
+  
+			<!-- Draw data points -->
+			<g>
+			  <polygon
+				:points="getPolygonPoints(chartData.data)"
+				fill="rgba(75, 192, 192, 0.2)"
+				stroke="rgba(75, 192, 192, 1)"
+			  />
+			  <polygon
+				:points="getPolygonPoints(chartData.data2)"
+				fill="rgba(192, 75, 192, 0.2)"
+				stroke="rgba(192, 75, 192, 1)"
+			  />
+			  <circle
+				v-for="(dataPoint, index) in chartData.data"
+				:key="index"
+				:cx="getCoordinate(chartRadius * (dataPoint / maxDataValue), index, chartData.data.length)[0]"
+				:cy="getCoordinate(chartRadius * (dataPoint / maxDataValue), index, chartData.data.length)[1]"
+				:r="pointRadius"
+				fill="rgba(75, 192, 192, 1)"
+			  />
+			  <circle
+				v-for="(dataPoint, index) in chartData.data2"
+				:key="index"
+				:cx="getCoordinate(chartRadius * (dataPoint / maxDataValue), index, chartData.data2.length)[0]"
+				:cy="getCoordinate(chartRadius * (dataPoint / maxDataValue), index, chartData.data2.length)[1]"
+				:r="pointRadius"
+				fill="rgba(192, 75, 192, 1)"
+			  />
+			</g>
+		  </g>
+		</svg>
+	  </div>
+	</div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+  
 	data() {
-		return {
-			isAllLevelsCompleted: false,
-			lineIndex: -1,
-			perspectives: {
-				biz: {},
-				dev: {},
-				ops: {}
-			},
-			initPerspectives: {	
-				biz: {
-					partIndex: 0,
-					partName: 'Biz',
-					partColor: 'rgba(100, 149, 237, 1)',
-					details: [
-						{
-							criteria: '1',
-							levels: [
-								{
-									Objective: '문서기반 요건분석',
-									checkpoints: [
-										{ text: '문서기반 요건분석', checked: false },
-										{ text: '요구사항 관리도구(e.g.Jira)의 활용', checked: false },
-										{ text: '유즈케이스 식별', checked: false },
-										{ text: '의존성 분석', checked: false },
-									],
-									isCompleted: false,
-								},
-								{
-									checkpoints: [
-										{ text: '인터페이스 설계', checked: false },
-										{ text: '데이터 액세스 계획', checked: false },
-										{ text: '보안 및 인증 고려', checked: false },
-										{ text: '에러 핸들링 정의', checked: false },
-									],
-									isCompleted: false,
-								},
-							],
-						},
-						{
-							criteria: '2',
-							levels: [
-								{
-									Objective: '문서기반 요건분석',
-									checkpoints: [
-										{ text: '문서기반 요건분석', checked: false },
-										{ text: '요구사항 관리도구(e.g.Jira)의 활용', checked: false },
-										{ text: '유즈케이스 식별', checked: false },
-										{ text: '의존성 분석', checked: false },
-									],
-									isCompleted: false,
-								},
-								{
-									checkpoints: [
-										{ text: '인터페이스 설계', checked: false },
-										{ text: '데이터 액세스 계획', checked: false },
-										{ text: '보안 및 인증 고려', checked: false },
-										{ text: '에러 핸들링 정의', checked: false },
-									],
-									isCompleted: false,
-								},
-							],
-						},
-						// 여기에 추가적인 비즈니스 요건 분석 정보를 넣으세요.
-					],
-				},
-				dev: {
-					partIndex: 1,
-					partName: 'Dev',
-					partColor: 'rgba(60, 179, 113, 1)',
-					details: [
-						{
-							criteria: '3',
-							levels: [
-								{
-									Objective: '문서기반 요건분석',
-									checkpoints: [
-										{ text: '문서기반 요건분석', checked: false },
-										{ text: '요구사항 관리도구(e.g.Jira)의 활용', checked: false },
-										{ text: '유즈케이스 식별', checked: false },
-										{ text: '의존성 분석', checked: false },
-									],
-									isCompleted: false,
-								},
-								{
-									checkpoints: [
-										{ text: '인터페이스 설계', checked: false },
-										{ text: '데이터 액세스 계획', checked: false },
-										{ text: '보안 및 인증 고려', checked: false },
-										{ text: '에러 핸들링 정의', checked: false },
-									],
-									isCompleted: false,
-								},
-							],
-						},
-						// 여기에 추가적인 개발 요건 분석 정보를 넣으세요.
-					],
-				},
-				ops: {
-					partIndex: 2,
-					partName: 'Ops',
-					partColor: 'rgba(138, 43, 226, 1)',
-					details: [
-						{
-							criteria: '4',
-							levels: [
-								{
-									checkpoints: [
-										{ text: '트랜잭션 관리', checked: false },
-										{ text: '데이터 일관성 유지', checked: false },
-									],
-									isCompleted: false,
-								},
-								{
-									checkpoints: [
-										{ text: '개별 스키마 정의', checked: false },
-										{ text: '다중 엔터프라이즈 데이터 조정', checked: false },
-										{ text: '데이터 일관성 유지', checked: false },
-									],
-									isCompleted: false,
-								},
-							],
-						},
-						// 여기에 추가적인 운영 요건 분석 정보를 넣으세요.
-					],
-				},
-			},
-			chartWidth: 440,
-			chartHeight: 440,
-			chartCenterX: 200,
-			chartCenterY: 200,
-			chartRadius: 150,
-			labelOffset: 20,
-			maxDataValue: 5,
-			pointRadius: 4,
-		};
-	},
-	mounted() {
-		this.loadCheckpointsState();
+	  return {
+		 isAllLevelsCompleted: false,
+		  perspectives: [
+		  {
+			name: '기능분해관점 체크포인트',
+			levels: [
+			  {
+				checkpoints: [
+					{ name: '기능과 유즈케이스 단위로 분리'},
+				  { text: '비즈니스 역량 도출: 비즈니스 도메인을 분석하여 주요 기능과 유즈케이스를 도출하였는가?', checked: false },
+				  { text: '기능 정의: 각 기능을 명확하게 정의하고 해당 기능의 범위를 결정하였는가?', checked: false },
+				  { text: '유즈케이스 식별: 각 기능에 대한 유즈케이스를 식별하고 이를 단위로 분리하였는가?', checked: false },
+				  { text: '의존성 분석: 각 기능과 유즈케이스 간의 의존성을 분석하여 적절한 분리 수준을 결정하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: '인터페이스 설계: 각 유즈케이스와 관련된 데이터에 대한 인터페이스를 정의하고 필요한 메소드와 매개변수를 명시하였는가?', checked: false },
+				  { text: '데이터 액세스 계획: 각 유즈케이스에서 필요한 데이터 액세스 방법을 계획하고 이를 인터페이스에 반영하였는가?', checked: false },
+				  { text: '보안 및 인증 고려: 서비스 간의 인터페이스에서 보안 및 인증 요구사항을 고려하여 액세스 제어 방법을 정의하였는가?', checked: false },
+				  { text: '에러 핸들링 정의: 인터페이스에서 발생할 수 있는 예외 상황에 대한 처리 방법과 오류 핸들링 전략을 정의하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: 'Ubiquitous language 정의: 각 bounded context에서 사용될 공통 언어와 용어를 정의하고 이를 문서화하였는가?', checked: false },
+				  { text: 'Boundary 정의: 각 bounded context의 경계를 명확하게 정의하고 외부에서의 커뮤니케이션 방식을 결정하였는가?', checked: false },
+				  { text: 'Anti-corruption layer 구현: bounded context 간의 통신을 관리하기 위해 Anti-corruption layer를 구현하고 외부 인터페이스 변환 및 데이터 변환을 수행하였는가?', checked: false },
+				  { text: '도메인 이벤트 식별: bounded context 간에 필요한 도메인 이벤트를 식별하고 이를 통해 비동기적인 상호작용을 구현하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: 'CQRS 모델 설계: 읽기와 쓰기를 위한 별도의 모델을 설계하고 도메인 이벤트를 활용하였는가?', checked: false },
+				  { text: '이벤트 스토리지 구현: 도메인 이벤트를 저장하고 관리하기 위한 이벤트 스토리지를 구현하였는가?', checked: false },
+				  { text: '이벤트 드리븐 아키텍처 구축: 도메인 이벤트를 기반으로 비즈니스 로직을 처리하는 이벤트 드리븐 아키텍처를 구축하였는가?', checked: false },
+				  { text: '이벤트 버전 관리: 도메인 이벤트의 버전을 관리하고 업데이트할 수 있는 메커니즘을 도입하였는가?',checked: false },
+				  { text: '이벤트 소싱 및 재생: 이벤트 소싱을 통해 시스템 상태를 재생하고 이벤트 스트림을 통해 상태 변경을 추적하였는가?',checked: false },
+				],
+				isCompleted: false,
+			  },
+			],
+		  },
+		  {
+			name: '데이터 관점 체크포인트',
+			levels: [
+			  {
+				checkpoints: [
+				  { text: '트랜잭션 관리: 각 마이크로서비스에서 ACID 기반의 트랜잭션을 유지하기 위한 방법과 프레임워크를 도입하였는가?', checked: false },
+				  { text: '데이터 일관성 유지: Canonical Data Model을 도입하여 데이터의 일관성을 유지하고 데이터의 중복 및 모순을 방지하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: '개별 스키마 정의: 각 마이크로서비스는 자체적인 스키마를 가지고 있으며, 각 서비스의 데이터 구조와 필드 정의가 명확하게 이루어졌는가?', checked: false },
+				  { text: '다중 엔터프라이즈 데이터 조정: 서비스 간의 다중 엔터프라이즈 데이터 저장소와의 트랜잭션 조정 방식을 정의하고 이를 구현하였는가?', checked: false },
+				  { text: '데이터 일관성 유지: 데이터 조정 및 동기화 과정에서 데이터 일관성을 유지하기 위한 메커니즘을 도입하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: '분산 데이터 관리: 각 마이크로서비스는 완전히 분산된 데이터 관리를 수행하고 있으며, 필요에 따라 데이터의 파티셔닝, 샤딩, 복제 등을 구현하였는가?', checked: false },
+				  { text: '폴리글랏 퍼시스턴스: 각 마이크로서비스는 자체적으로 다른 유형의 DBMS를 사용할 수 있는 폴리글랏 퍼시스턴스를 구현하였는가?', checked: false },
+				  { text: '데이터 접근 및 관리: 각 마이크로서비스에서 필요한 데이터 액세스 방법을 선택하고 이를 효율적으로 관리하고 있으며, 성능과 확장성을 고려하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			  {
+				checkpoints: [
+				  { text: '이벤트 기반 데이터 관리: 이벤트 중심 아키텍처를 구축하여 각 마이크로서비스 간의 데이터 흐름을 이벤트 기반으로 관리하고 있는가?', checked: false },
+				  { text: '이벤트 소싱 및 커맨드-쿼리 분리(CQRS): 이벤트 소싱을 통해 시스템의 상태를 재생하고 커맨드와 쿼리를 분리하여 비동기적인 데이터 처리를 구현하였는가?', checked: false },
+				  { text: '데이터 일관성 관리: 일시적으로 데이터에 일관성이 없는 상태가 존재할 수 있으며, 이를 일정 시간 내에 해소하여 다시 일관성을 충족시키는 메커니즘을 구현하였는가?', checked: false },
+				],
+				isCompleted: false,
+			  },
+			],
+		  },
+		  // Add more perspectives here if needed
+		],
+  
+		chartData: {
+		  labels: ['기능분해', '데이터', 'SW 아키텍처', 'Infra', '스트럭처', '배포', '팀구조'],
+		  data: [0, 0, 0, 0, 0, 0, 0],
+		  data2: [0, 0, 0, 0, 0, 0, 0],
+		},
+		chartWidth: 400,
+		chartHeight: 400,
+		chartCenterX: 200,
+		chartCenterY: 200,
+		chartRadius: 150,
+		labelOffset: 20,
+		maxDataValue: 5,
+		pointRadius: 4,
+	  };
 	},
 	methods: {
-		selectedTree(item) {
-			this.lineIndex = -1;
-			if( item.perspectives == null ) {
-				this.perspectives = _.cloneDeep(this.initPerspectives)
-				item.perspectives = this.perspectives
-			} else {
-				this.perspectives = item.perspectives
-			}
-		},
-		getCoordinate(radius, index, total) {
-			const angle = (Math.PI * 2 * index) / total - Math.PI / 2;
-			const x = radius * Math.cos(angle);
-			const y = radius * Math.sin(angle);
-			return [x, y];
-		},
-		getPolygonPoints(data) {
-			return data
-			.map((dataPoint, index) => {
-				const radius = this.chartRadius * (dataPoint / this.maxDataValue);
-				return this.getCoordinate(radius, index, data.length).join(',');
-			}).join(' ');
-		},
-		updateLevelCompletion(perspective, level) {
-			level.isCompleted = level.checkpoints.every(checkpoint => checkpoint.checked);
-			this.checkAllLevelsCompletion();
-			this.changeCheckpointsState();
-		},
-		changeCheckpointsState() {
-			Object.values(this.perspectives).forEach(part => {
-				part.details.forEach(detail => {
-					const criteria = detail.criteria;
-					const levels = detail.levels.map(level => ({
-						checkpoints: level.checkpoints.map(checkpoint => {
-							if (checkpoint.checked) {
-								this.$emit('changeCheckpointSave');
-							}
-							return checkpoint;
-						})
-					}));
-				});
-			});
-		},
-		loadCheckpointsState() {
-			const savedState = localStorage.getItem(LOCAL_STORAGE_KEY);
-			if (savedState) {
-				const checkpointsState = JSON.parse(savedState);
-				Object.values(this.perspectives).forEach((part, pIndex) => {
-					const savedPart = checkpointsState[Object.keys(this.perspectives)[pIndex]];
-					if (savedPart) {
-						part.details.forEach((detail, lIndex) => {
-							detail.levels.forEach((level, cIndex) => {
-								level.checkpoints.forEach((checkpoint, ckIndex) => {
-									checkpoint.checked = savedPart.details[lIndex].levels[cIndex].checkpoints[ckIndex].checked;
-								});
-								this.updateLevelCompletion(detail, level);
-							});
-						});
-					}
-				});
-			} else {
-				// localStorage에 데이터가 없을 경우 initPerspectives로 초기화
-				this.perspectives = _.cloneDeep(this.initPerspectives);
-			}
-		},
-		checkAllLevelsCompletion() {
-			this.isAllLevelsCompleted = this.getAllDetail(this.perspectives).every(detailObj => 
-				detailObj.detail.levels.every(level => level.isCompleted)
-			);
-		},
-		getDataArray(perspectives) {
-			return this.getAllDetail(perspectives).map(detail => {
-				let completedLevelsCount = 0;
-				if (detail.detail && detail.detail.levels) {
-					detail.detail.levels.forEach(level => {
-						if (level.isCompleted) {
-							completedLevelsCount += 1;
-						}
-					});
-				}
-				return completedLevelsCount;
-			});
-		},
-		getDataLength(perspectives) {
-			let count = 0;
-			Object.keys(perspectives).forEach(key => {
-				var perspective = perspectives[key]
-				perspective.details.forEach(detail => {
-					count += 1;
-				});
-			});
-			return count;
-		},
-		getAllDetail(perspectives) {
-			var allDetail = [];
-			Object.keys(perspectives).forEach(key => {
-				var perspective = perspectives[key];
-				if(perspective.details != null) {
-					perspective.details.forEach(detail => {
-						allDetail.push({ 
-							detail: detail,
-							color: perspective.partColor
-						});
-					});
-				}
-			});
-			return allDetail;
-		},
-	}
-};
-</script>
+	  getCoordinate(radius, index, total) {
+		const angle = (Math.PI * 2 * index) / total - Math.PI / 2;
+		const x = radius * Math.cos(angle);
+		const y = radius * Math.sin(angle);
+		return [x, y];
+	  },
+	  getPolygonPoints(data) {
+		return data
+		  .map((dataPoint, index) => {
+			const radius = this.chartRadius * (dataPoint / this.maxDataValue);
+			return this.getCoordinate(radius, index, data.length).join(',');
+		  })
+		  .join(' ');
+	  },
+	  updateLevelCompletion(perspective, level) {
+		level.isCompleted = level.checkpoints.every(checkpoint => checkpoint.checked);
+		this.checkAllLevelsCompletion(perspective);
+	  },
+   checkAllLevelsCompletion(perspective) {
+	perspective.isCompleted = perspective.levels.every((level) => level.isCompleted);
   
-<style>
-
-</style>
+  //  if (perspective.isCompleted) {
+	  const perspectiveIndex = this.chartData.labels.findIndex((label) => label === perspective.name);
+	  if (perspectiveIndex !== -1) {
+		const lastCompletedLevelIndex = perspective.levels.findIndex((level) => !level.isCompleted) - 1;
+		const value = lastCompletedLevelIndex !== -1 ? lastCompletedLevelIndex + 1 : this.chartData.labels.length;
+		this.chartData.data[perspectiveIndex] = value;
+		this.chartData.data2[perspectiveIndex] = value;
+	  }
+	//}
+  },
+	}
+  };
+  </script>
+  
+  <style>
+  
+  .container {
+	display: flex;
+	justify-content: space-between;
+  }
+  
+  .checkpoints-container {
+	flex: 1;
+  }
+  
+  .chart-container {
+	flex: 1;
+  }
+  svg {
+	display: block;
+	margin: auto;
+  }
+  </style>
   
   

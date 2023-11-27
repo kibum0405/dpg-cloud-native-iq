b54186a2292c1357ac887038026c8d23ab38c92d
Ju <tjdus0735@naver.com>
2023-11-27 18:28:00 +0900

    'step.vue,user.vue'

--- src/components/spider/step/User.vue
+++ src/components/spider/step/User.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
       <!-- 사용자 선택 -->
-      <h1>프로필 등록</h1>
-
-      <div>사용자 프로필을 선택하거나 등록하세요.</div>
-      <v-select
-        :items="users"
-        :item-text="userDisplayText"
-        item-value="email"
-        label="사용자 선택"
-        v-model="selectedUser"
-      ></v-select>
-  
-      <!-- 등록 버튼 -->
-      <v-btn color="primary" @click="showDialog = true">사용자 등록</v-btn>
+      <div class="user-box">
+        <h2>프로필 선택/등록</h2>
+        <!-- <div class="sub-tit">사용자 프로필을 선택하거나 등록하세요.</div> -->
+        
+        <div class="user-box-in">
+            <v-select
+                :items="users"
+                :item-text="userDisplayText"
+                item-value="email"
+                label="사용자 선택"
+                v-model="selectedUser"
+                class="ml10"
+            ></v-select>
+        
+            <!-- 등록 버튼 -->
+            <v-btn color="primary" @click="showDialog = true" class="btn-reg">사용자 등록</v-btn>
+        </div>
+      </div>
+      
   
       <!-- 등록 다이얼로그 -->
       <v-dialog v-model="showDialog" persistent max-width="600px">
@@ -27,6 +33,7 @@
                                 v-model="newUser.name"
                                 :rules="newUser.nameRules"
                                 required
+                                style="width:300px;"
                             ></v-text-field>
                             <v-text-field class="user-input-field"
                                 label="이메일"
@@ -40,8 +47,12 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="closeDialog">취소</v-btn>
-                    <v-btn color="green darken-1" text @click="registerUser">등록</v-btn>
+                    <!-- <v-btn color="blue darken-1" text @click="closeDialog">취소</v-btn>
+                    <v-btn color="green darken-1" text @click="registerUser">등록</v-btn> -->
+                    <div class="reg-btn-box">
+                        <v-btn color="primary" @click="registerUser">등록</v-btn>
+                        <v-btn style="margin-left: 10px;border:1px solid #ddd;" text @click="closeDialog">취소</v-btn>
+                    </div>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -86,8 +97,61 @@
     }
 </script>
 <style>
+.user-box {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    border-bottom: 1px dashed #ccc;
+    /* border-radius: 5px;
+    padding: 10px; */
+    /* box-shadow: 0 4px 4px -4px #333; */
+    /* box-shadow: 0 1px 2px 1px #ddd; */
+}
+.user-box-in {
+    width: 86%;
+    display: flex;
+    align-items: center;
+}
+.ml10, .btn-reg {
+    margin-left: 10px;
+}
 .user-input-field .v-messages__message {
     line-height: 18px;
 }
+.reg-btn-box {
+    margin: 0 10px 10px 0;
+}
+
+@media only screen and (max-width:1440px) {
+    .user-box > h2 {
+        font-size: 22px;
+    }
+    .user-box-in {
+        width: 82%;
+    }
+}
+@media only screen and (max-width:1024px) {
+    .user-box-in {
+        width: 75%;
+    }
+}
+@media only screen and (max-width:750px) {
+  .user-box {
+    height: 90px;
+    display: block;
+  }
+  .user-box-in {
+    width: 100%;
+  }
+  .ml10 {
+    margin: 0;
+  }
+  .btn-reg {
+    margin-top: -10px;
+  }
+} 
 </style>
   
